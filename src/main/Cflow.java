9462c5786e76630296d02fd865032b4ae7a74cf1
Ricardo Silva <ricardani@gmail.com>
2014-04-29 16:39:39 +0100

    Adicionado no NFA do Cflow.java o OPTIONAL, PLUS, ANY e STAR Ainda ha erros ao fazer (("A""B")?|"C") em que o vertice final é diferente, o erro deve se encontrar na parte do OPTIONAL

--- src/main/Cflow.java
+++ src/main/Cflow.java
@@ -1,5 +1,7 @@
 package main;
 
+
+import java.util.*;
 import parser.SimpleNode;
 import parser.SimplePCRE;
 import parser.SimplePCRETreeConstants;
@@ -7,7 +9,9 @@ import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.Graph;
 
 public class Cflow {
-	int vIndex;
+	int             vIndex;
+	final String    EPSILON = "\u03B5";
+	Vector<Integer> groups  = new Vector<Integer>();
 
 	public Cflow() {
 		vIndex = 0;
@@ -42,11 +46,12 @@ public class Cflow {
 
 	private Graph<Integer, EdgeString> generateGraph(SimpleNode ast) {
 		Graph<Integer, EdgeString> g = new DirectedSparseMultigraph<Integer, EdgeString>();
+
 		g.addVertex((Integer) vIndex++);
 		g.addVertex((Integer) vIndex++);
 		// TODO ver primeiro caso
 		SimpleNode n = (SimpleNode) ast.jjtGetChild(0);
-		parseNode(g, 0, 1, n);
+		parseNode(g, 1, 0, n);
 
 		return g;
 	}
@@ -56,33 +61,91 @@ public class Cflow {
 		switch (actual.getId()) {
 
 		case SimplePCRETreeConstants.JJTIDENTIFIER:
-			g.addEdge(new EdgeString((String) actual.jjtGetValue()), anterior,
-					seguinte);
+			String value = (String) actual.jjtGetValue();
+			g.addEdge(new EdgeString(value), anterior, seguinte);
 			break;
 
 		case SimplePCRETreeConstants.JJTSEQUENCE:
 			nChild = actual.jjtGetNumChildren();
 			int end = seguinte;
+
 			for (int i = 0; i < nChild; i++) {
-				if (i != nChild - 1) {
+
+				SimpleNode n = (SimpleNode) actual.jjtGetChild(i);
+
+				if (i != nChild - 1 && n.getId() != SimplePCRETreeConstants.JJTOPTIONAL) {
 					g.addVertex((Integer) vIndex++);
 					seguinte = vIndex - 1;
 				}
-				SimpleNode n = (SimpleNode) actual.jjtGetChild(i);
-				if (i == nChild - 1)
-					seguinte = end;
+
+				if (i == nChild - 1) {
+					
+					if (g.getIncidentEdges(end).size() == 0 && (n.getId() == SimplePCRETreeConstants.JJTOPTIONAL)){
+						g.removeVertex(end);
+					}else{
+						seguinte = end;
+					}
+				}
+
+				groups.add(anterior);
 				parseNode(g, anterior, seguinte, n);
 
 				anterior = seguinte;
 
 			}
+
+			for (int i = 0; i < nChild; i++) {
+				groups.remove(groups.size() - 1);
+			}
+
 			break;
+
 		case SimplePCRETreeConstants.JJTUNION:
 			nChild = actual.jjtGetNumChildren();
+
+			groups.add(anterior);
 			for (int i = 0; i < nChild; i++) {
 				SimpleNode n = (SimpleNode) actual.jjtGetChild(i);
 				parseNode(g, anterior, seguinte, n);
 			}
+			groups.remove(groups.size() - 1);
+			break;
+
+		case SimplePCRETreeConstants.JJTANY:
+
+			g.addEdge(new EdgeString("Any"), anterior, seguinte);
+
+			break;
+
+		case SimplePCRETreeConstants.JJTOPTIONAL:
+
+			g.addEdge(new EdgeString("Optional"), groups.get(groups.size() - 2), anterior);
+
+			break;
+
+		case SimplePCRETreeConstants.JJTPLUS:
+
+			Collection<EdgeString> c = g.getInEdges(anterior);
+
+			Iterator<EdgeString> itr = c.iterator();
+			EdgeString lastElement = itr.next();
+			while (itr.hasNext()) {
+				lastElement = itr.next();
+			}
+			String edgeName = lastElement.getValue();
+
+			g.addEdge(new EdgeString("Repeat: " + edgeName), anterior, groups.get(groups.size() - 1));
+			g.addEdge(new EdgeString("Epsilon"), anterior, seguinte);
+
+			break;
+
+		case SimplePCRETreeConstants.JJTSTAR:
+
+			g.addEdge(new EdgeString("Star"), anterior, groups.get(groups.size() - 2));
+			g.addEdge(new EdgeString("Epsilon"), groups.get(groups.size() - 2), seguinte);
+			g.addEdge(new EdgeString("Epsilon"), anterior, seguinte);
+
+			break;
 		}
 
 	}
